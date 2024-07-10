15dbf07247e761ed594981efe4ca1c53af793c0f
Bidouda <59067642+Bidouda@users.noreply.github.com>
2024-07-10 12:22:48 -0300

    Solve exercise 04_removeFromArray

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,12 @@
-const removeFromArray = function() {
-};
+const removeFromArray = function(list, ...listItems) {
+  for(const listItem of listItems){
+    while(list.includes(listItem)){
+      let position = list.indexOf(listItem);
+      list.splice(position, 1);
+    }
+  }
+  return list;
+}; 
 
 // Do not edit below this line
 module.exports = removeFromArray;
