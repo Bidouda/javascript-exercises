65bb443655afb4e20c63809eb57a291dfd1cfc66
Bidouda <59067642+Bidouda@users.noreply.github.com>
2024-07-10 11:53:31 -0300

    Solve exercise 03_reverseString

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,8 @@
-const reverseString = function() {
-
+const reverseString = function(phrase) {
+  phrase = phrase.split("");
+  phrase = phrase.reverse();
+  phrase = phrase.join("");
+  return phrase;
 };
 
 // Do not edit below this line
