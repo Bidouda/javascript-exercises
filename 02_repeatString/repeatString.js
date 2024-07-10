f97ce4be0f2e2f9be93f6193b430f53b3e3f3e4b
Bidouda <59067642+Bidouda@users.noreply.github.com>
2024-07-10 11:42:52 -0300

    Solve exercise 02_repeatString

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,12 @@
-const repeatString = function() {
-
+const repeatString = function(string, num) {
+  if(num < 0){
+    return "ERROR";
+  }
+  response = "";
+  for(let i = 0; i < num; i++){
+    response += string;
+  }
+  return response;
 };
 
 // Do not edit below this line
